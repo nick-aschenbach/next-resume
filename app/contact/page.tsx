d49c7f6fd73a49c0cd59fb3e585a4dba6308d8d8
Nick Aschenbach <nick.aschenbach@gmail.com>
2024-01-09 01:32:08 -0800

    Touch up spacing for icons

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -8,12 +8,12 @@ import { GoLink } from 'react-icons/go'
 export default function ContactInfo() {
   return (
     <div className={'m-10'}>
-      <div className={'md:grid md:grid-cols-6 md:gap-1'}>
+      <div className={'md:grid md:grid-cols-6 md:gap-2'}>
         <div className={'col-span-1'}>Email</div>
         <div className={'col-span-5'}>
           <div className={'flex nav-link-light'}>
-            <MdOutlineMailOutline size={24} />
-            <div className={'pl-2'}>
+            <MdOutlineMailOutline size={24} className={'absolute'} />
+            <div className={'pl-8'}>
               <Link href={'mailto:nick.aschenbach@gmail.com'}>nick.aschenbach@gmail.com</Link>
             </div>
           </div>
@@ -21,8 +21,8 @@ export default function ContactInfo() {
         <div className={'col-span-1'}>Phone</div>
         <div className={'col-span-5'}>
           <div className={'flex nav-link-light'}>
-            <HiOutlineDevicePhoneMobile size={24} />
-            <div className={'pl-2'}>
+            <HiOutlineDevicePhoneMobile size={24} className={'absolute'}/>
+            <div className={'pl-8'}>
               <Link href={'tel:503-915-1358'}>(503) 915.1358 (US)</Link>
             </div>
           </div>
@@ -30,8 +30,8 @@ export default function ContactInfo() {
         <div className={'col-span-1'}>LinkedIn</div>
         <div className={'col-span-5'}>
           <div className={'flex nav-link-light'}>
-            <CiLinkedin size={24} />
-            <div className={'pl-2'}>
+            <CiLinkedin size={24} className={'absolute'} />
+            <div className={'pl-8'}>
               <Link href={'https://www.linkedin.com/in/nick-aschenbach-87905b5/'} target={'_blank'}>
                 https://www.linkedin.com/in/nick-aschenbach-87905b5/
               </Link>
@@ -41,8 +41,8 @@ export default function ContactInfo() {
         <div className={'col-span-1'}>Blog</div>
         <div className={'col-span-5'}>
           <div className={'flex nav-link-light'}>
-            <GoLink size={18} />
-            <div className={'pl-2'}>
+            <GoLink size={20} className={'absolute pl-1'}/>
+            <div className={'pl-8'}>
               <Link href={'https://nick-aschenbach.github.io'} target={'_blank'}>
                 https://nick-aschenbach.github.io
               </Link>
